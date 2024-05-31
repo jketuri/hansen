2ca3ba81e283eab53eb4c4428f78d6b02b1b6584
unknown <j>
2024-05-31 12:30:31 +0300

    comments and logging

--- programming_task/soft_tough.py
+++ programming_task/soft_tough.py
@@ -1,59 +1,82 @@
 #!/usr/bin/python
 # coding=utf-8
 
-from logging import Logger
 from argparse import ArgumentParser
+import logging
+import re
 
-LETTER_WORDS = {'S': 'Soft', 'T': 'Tough'}
+_LETTER_WORDS = {'S': 'Soft', 'T': 'Tough'}
+
+_LOGGER = logging.getLogger(__name__)
 
 class SoftTough:
 
-    def convert_to_words(
+    def _convert_to_words(
             self,
             letter_codes: str,
             number: int
     ) -> list[str]:
+        ''' Converts ST-codes to a word list '''
+        _LOGGER.info(
+            'Converting %s to a word list with number %d', letter_codes,
+            number)
         return list(map(
-            lambda letter: LETTER_WORDS[letter], letter_codes[:number]))
+            lambda letter: _LETTER_WORDS[letter], letter_codes[:number]))
 
 
-    def convert_to_sentence(
+    def _convert_to_sentence(
             self,
             letter_codes: str,
             number: int
     ):
+        ''' Converts ST-codes to a sentence '''
+        _LOGGER.info(
+            'Converting %s to a sentence with number %d', letter_codes, number)
         words = []
         while len(words) < number:
             words.extend(
-                self.convert_to_words(
+                self._convert_to_words(
                     letter_codes, number - len(words)))
         return ', '.join(
             words[:-1]) + f' and {words[-1]}.'
 
 
-    def convert_to_text(
+    def _convert_to_text(
             self,
             letter_codes: str,
             numbers: list[int]
     ):
+        ''' Converts letter codes to a text '''
+        _LOGGER.info(
+            'Converting %s to a text with numbers %s', letter_codes, numbers)
         sentences = []
         for number in numbers:
             sentences.append(
-                self.convert_to_sentence(letter_codes, number))
+                self._convert_to_sentence(letter_codes, number))
         return '\n'.join(sentences)
 
 
+def _validate_pattern(
+        pattern: str
+) -> str:
+    if not re.match('^[ST]*$', pattern):
+        raise ValueError
+    return pattern
+
+
 def soft_tough():
+    ''' Converts letter codes to a text '''
     parser = ArgumentParser(
         prog='soft_tough', description='The application converts '
         'each character in a pattern with letters S or T into '
         'human readable text and output the corresponding text.')
-    parser.add_argument('pattern')
+    parser.add_argument('pattern', type=_validate_pattern)
     parser.add_argument('number', nargs='+', type=int)
     args = parser.parse_args()
-    print(SoftTough().convert_to_text(
+    print(SoftTough()._convert_to_text(
         letter_codes=args.pattern, numbers=args.number))
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     soft_tough()
--- programming_task/soft_tough_test.py
+++ programming_task/soft_tough_test.py
@@ -10,19 +10,19 @@ class TestSoftTough(TestCase):
     def test_convert_to_words(self):
         soft_tough = SoftTough()
         self.assertEqual(
-            soft_tough.convert_to_words('SST', 3),
+            soft_tough._convert_to_words('SST', 3),
             ['Soft', 'Soft','Tough'])
         
     def test_convert_to_sentence(self):
         soft_tough = SoftTough()
         self.assertEqual(
-            soft_tough.convert_to_sentence('SST', 5),
+            soft_tough._convert_to_sentence('SST', 5),
             'Soft, Soft, Tough, Soft and Soft.')
 
     def test_convert_to_text(self):
         soft_tough = SoftTough()
         self.assertEqual(
-            soft_tough.convert_to_text('SST', [5, 2]),
+            soft_tough._convert_to_text('SST', [5, 2]),
             'Soft, Soft, Tough, Soft and Soft.\nSoft and Soft.')
 
 if __name__ == '__main__':
